644fb79442baa381288152a796b0b38a9806429c
Alessandro Zini <alezini94@hotmail.it>
2025-06-27 14:34:58 +0200

    chore: formatting
    Used black for formatting and ignored flake8 line lenght bs

--- velox.py
+++ velox.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# flake8: noqa: E501
+
 import argparse
 import asyncio
 import json
@@ -13,7 +15,7 @@ from apscheduler.triggers.cron import CronTrigger
 from bs4 import BeautifulSoup
 from telegram import Update
 from telegram.constants import ParseMode
-from telegram.ext import (Application, ApplicationBuilder, CommandHandler, ContextTypes)
+from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -24,31 +26,31 @@ def generate_maps_base_url(lat_long_t):
 
 def fetch_current_dict():
     """Fetch the current velox list and returns it as a {location_name:maps_url} dict"""
-    url = 'https://polizei.lu.ch/organisation/sicherheit_verkehrspolizei/verkehrspolizei/spezialversorgung/verkehrssicherheit/Aktuelle_Tempomessungen'
+    url = "https://polizei.lu.ch/organisation/sicherheit_verkehrspolizei/verkehrspolizei/spezialversorgung/verkehrssicherheit/Aktuelle_Tempomessungen"
 
-    response = requests.get(url, timeout=30, headers={'User-Agent': 'Mozilla/5.0'})
+    response = requests.get(url, timeout=30, headers={"User-Agent": "Mozilla/5.0"})
     if response.status_code != 200:
         print(f"Failed to make request. Status code: {response.status_code}")
         return None
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    radar_list_div = soup.find('div', {'id': 'radarList'})
+    soup = BeautifulSoup(response.text, "html.parser")
+    radar_list_div = soup.find("div", {"id": "radarList"})
     if not radar_list_div:
         print("Could not find div with id 'radarList'")
         return None
 
-    li_tags = radar_list_div.find_all('li')
+    li_tags = radar_list_div.find_all("li")
 
     # exclude the last <li> tag since it is a (rather useless) link to the map itself
     li_tags = li_tags[:-1]
 
     current_dict = {}
     for li in li_tags:
-        a_tag = li.find('a')
+        a_tag = li.find("a")
 
         # extract and store the text content and coordinates
         if a_tag:
-            match = re.search(r"map\.flyTo\(\[(.*?),(.*?)\]", a_tag.get('onclick', ''))
+            match = re.search(r"map\.flyTo\(\[(.*?),(.*?)\]", a_tag.get("onclick", ""))
             if match:
                 lat = match.group(1).strip()
                 long = match.group(2).strip()
@@ -63,7 +65,7 @@ def fetch_current_dict():
 
 
 def save_chats(chat_ids):
-    with open(f'{BASE_DIR}/chat_ids.json', 'w', encoding='utf-8') as f:
+    with open(f"{BASE_DIR}/chat_ids.json", "w", encoding="utf-8") as f:
         json.dump(chat_ids, f, indent=2)
 
 
@@ -72,7 +74,7 @@ def save_chat_id(chat_id):
     chat_id = str(chat_id)
 
     try:
-        with open(f'{BASE_DIR}/chat_ids.json', 'r', encoding='utf-8') as f:
+        with open(f"{BASE_DIR}/chat_ids.json", "r", encoding="utf-8") as f:
             chat_ids = json.load(f)
     except FileNotFoundError:
         chat_ids = {}
@@ -89,7 +91,7 @@ def save_chat_id(chat_id):
 
 def get_chats():
     try:
-        with open(f'{BASE_DIR}/chat_ids.json', 'r', encoding='utf-8') as f:
+        with open(f"{BASE_DIR}/chat_ids.json", "r", encoding="utf-8") as f:
             return json.load(f)
     except FileNotFoundError:
         # no previous users
@@ -116,39 +118,41 @@ async def broadcast(app, msg, no_updates):
         chat_id = str(chat_id)
         if no_updates and not should_notify_no_updates(chat_id):
             continue
-        await app.bot.send_message(chat_id=chat_id, text=msg,
-                                   parse_mode=ParseMode.HTML,
-                                   disable_web_page_preview=True)
+        await app.bot.send_message(
+            chat_id=chat_id,
+            text=msg,
+            parse_mode=ParseMode.HTML,
+            disable_web_page_preview=True,
+        )
 
 
 # command to handle /start
-async def cmd_start(update: Update,
-                    context: ContextTypes.DEFAULT_TYPE):
+async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.chat_id
     newly_subscribed = save_chat_id(chat_id)
     msg = "You're subscribed to updates."
     if not newly_subscribed:
         msg = "Already subscribed."
-    await context.bot.send_message(chat_id=chat_id,
-                                   text=msg)
+    await context.bot.send_message(chat_id=chat_id, text=msg)
 
 
 # command to handle /current_list
-async def cmd_current_list(update: Update,
-                           context: ContextTypes.DEFAULT_TYPE):
+async def cmd_current_list(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     msg = "Current List\n\n"
     for velox, lat_long_t in fetch_current_dict().items():
         msg += f"- <a href='{generate_maps_base_url(lat_long_t)}'>{velox}</a>\n"
 
-    await context.bot.send_message(chat_id=update.message.chat_id,
-                                   text=msg, parse_mode=ParseMode.HTML,
-                                   disable_web_page_preview=True)
+    await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text=msg,
+        parse_mode=ParseMode.HTML,
+        disable_web_page_preview=True,
+    )
 
 
 # command to handle /show_map
-async def cmd_show_map(update: Update,
-                       context: ContextTypes.DEFAULT_TYPE):
+async def cmd_show_map(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     url = "https://www.google.com/maps/dir/"
     # hardcoded coords of Luzern for map centering
@@ -160,20 +164,21 @@ async def cmd_show_map(update: Update,
 
     msg = f"Velox map\n{url}"
 
-    await context.bot.send_message(chat_id=update.message.chat_id,
-                                   text=msg, parse_mode=ParseMode.HTML,
-                                   disable_web_page_preview=True)
+    await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text=msg,
+        parse_mode=ParseMode.HTML,
+        disable_web_page_preview=True,
+    )
 
 
 # command to handle /manual_update
-async def cmd_manual_update(_update: Update,
-                            context: ContextTypes.DEFAULT_TYPE):
+async def cmd_manual_update(_update: Update, context: ContextTypes.DEFAULT_TYPE):
     return await check_for_updates(context.application, forced_update=True)
 
 
 # command to handle /notify_no_updates
-async def cmd_set_notify_no_updates(update: Update,
-                                    context: ContextTypes.DEFAULT_TYPE):
+async def cmd_set_notify_no_updates(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = str(update.message.chat_id)
     chat_ids = get_chats()
 
@@ -189,8 +194,7 @@ async def cmd_set_notify_no_updates(update: Update,
     if new_val:
         msg = "Enabled - get status updates even if no changes are detected"
 
-    await context.bot.send_message(chat_id=update.message.chat_id,
-                                   text=f"{msg}")
+    await context.bot.send_message(chat_id=update.message.chat_id, text=f"{msg}")
 
 
 async def check_for_updates(app=None, save_list=True, forced_update=False):
@@ -213,7 +217,7 @@ async def check_for_updates(app=None, save_list=True, forced_update=False):
 
     # load previous dict
     try:
-        with open(f'{BASE_DIR}/previous_dict.json', 'r', encoding='utf-8') as f:
+        with open(f"{BASE_DIR}/previous_dict.json", "r", encoding="utf-8") as f:
             previous_dict = json.load(f)
             set_previous = set(previous_dict.keys())
     except (FileNotFoundError, ValueError):
@@ -244,22 +248,24 @@ async def check_for_updates(app=None, save_list=True, forced_update=False):
 
     if not no_updates and save_list:
         # save the current list
-        with open(f'{BASE_DIR}/previous_dict.json', 'w', encoding='utf-8') as f:
+        with open(f"{BASE_DIR}/previous_dict.json", "w", encoding="utf-8") as f:
             json.dump(current_dict, f)
 
 
 def bot_start():
     # get the token from config.json
     try:
-        with open(f'{BASE_DIR}/config.json', 'r', encoding='utf-8') as f:
+        with open(f"{BASE_DIR}/config.json", "r", encoding="utf-8") as f:
             configs = json.load(f)
     except (FileNotFoundError, ValueError):
-        with open(f'{BASE_DIR}/config.json', 'w', encoding='utf-8') as f:
+        with open(f"{BASE_DIR}/config.json", "w", encoding="utf-8") as f:
             configs = {"BOT_TOKEN": ""}
             json.dump(configs, f, indent=2)
 
-        print("Error: no valid config.json found. A template has been created,"
-              "but you need to fill your bot's token.")
+        print(
+            "Error: no valid config.json found. A template has been created,"
+            "but you need to fill your bot's token."
+        )
         sys.exit(1)
 
     if configs["BOT_TOKEN"] in (None, ""):
@@ -268,16 +274,11 @@ def bot_start():
 
     app = ApplicationBuilder().token(configs["BOT_TOKEN"]).build()
 
-    app.add_handler(CommandHandler("start",
-                                   cmd_start))
-    app.add_handler(CommandHandler("current_list",
-                                   cmd_current_list))
-    app.add_handler(CommandHandler("manual_update",
-                                   cmd_manual_update))
-    app.add_handler(CommandHandler("notify_no_updates",
-                                   cmd_set_notify_no_updates))
-    app.add_handler(CommandHandler("show_map",
-                                   cmd_show_map))
+    app.add_handler(CommandHandler("start", cmd_start))
+    app.add_handler(CommandHandler("current_list", cmd_current_list))
+    app.add_handler(CommandHandler("manual_update", cmd_manual_update))
+    app.add_handler(CommandHandler("notify_no_updates", cmd_set_notify_no_updates))
+    app.add_handler(CommandHandler("show_map", cmd_show_map))
 
     async def post_init(application: Application):
         trigger = CronTrigger(
@@ -300,12 +301,18 @@ def bot_start():
 # entry point
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--telegram-bot', action='store_true',
-                    help='Start the Telegram bot')
-parser.add_argument('-s', '--save-list', action='store_true',
-                    help='[CLI] Save list when performing an update check')
-parser.add_argument('-p', '--print-list', action='store_true',
-                    help='[CLI] Print the current list')
+parser.add_argument(
+    "-t", "--telegram-bot", action="store_true", help="Start the Telegram bot"
+)
+parser.add_argument(
+    "-s",
+    "--save-list",
+    action="store_true",
+    help="[CLI] Save list when performing an update check",
+)
+parser.add_argument(
+    "-p", "--print-list", action="store_true", help="[CLI] Print the current list"
+)
 args = parser.parse_args()
 
 if args.telegram_bot:
